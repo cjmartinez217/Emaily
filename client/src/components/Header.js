c76e1c02ce032bca1ee7361ca85dc94531ff4658
cjmartinez217 <cjmartinez217@gmail.com>
2024-08-20 08:04:16 -0500

    action creator wip

---
+++ client/src/components/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <nav>
+        <div class="nav-wrapper">
+          <a class="left brand-logo">Emaily</a>
+          <ul class="right">
+            <li><a>Login with Google</a></li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
+}
+
+export default Header;
